d2b2878134500f018c59ff4bc4e0603374524ce7
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-27 00:27:36 +0100

    .

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/shared/navbar/page";
 import React from "react";
 
 const layout = ({ children }) => {
   return (
     <main>
+      <Navbar />
       <div>{children}</div>
     </main>
   );
