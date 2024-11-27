ad18c903491083a44b5fb29b9a70c5d702e5964e
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-11-27 23:43:46 +0100

    .

--- components/Hero/page.jsx
+++ components/Hero/page.jsx
@@ -7,9 +7,13 @@ const Hero = () => {
       style={{ backgroundImage: "url(/taxi.jpg)", zIndex: -1 }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10">
-        <div className=" flex justify-end">
-          <h1 className="flex flex-col lg:flex-row lg:gap-8 w-full text-[50px] lg:text-[110px] text-left justify-end pl-[40px] pt-[200px] lg:p-[150px] text-white">
-            <span className="text-cool-green  font-bold"> Cool </span>Taxi Niš
+        <div className="">
+          <h1 className="flex flex-col lg:flex-row lg:gap-8 w-full text-[60px] lg:text-[110px] text-right justify-end pr-[40px] pt-[200px] lg:p-[150px] leading-snug text-white">
+            <span className="text-cool-green font-bold pr-[80px] lg:pr-0">
+              {" "}
+              Cool{" "}
+            </span>
+            Taxi Niš
           </h1>
         </div>
       </div>
