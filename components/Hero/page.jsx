b7ecda46374bb7bbc3a56eac4fce323fba212342
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-02 19:21:07 +0100

    .

--- components/Hero/page.jsx
+++ components/Hero/page.jsx
@@ -15,24 +15,24 @@ const Hero = () => {
         }}
       ></div>
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
-      <div className="relative z-20 flex flex-col gap-5 items-start  pb-[15px] pt-[130px]">
+      <div className=" lg:hidden relative z-20 flex flex-col gap-5 items-start  pb-[15px] pt-[130px]">
         <a
           href="tel:+381691971111"
-          className="flex items-center ml-[60px] border-2 border-cool-green text-cool-green bg-black px-[12px] py-[4px] rounded-full"
+          className="flex items-center ml-[50px] border-2 border-cool-green text-white bg-black px-[12px] py-[4px] rounded-full"
         >
           <FcPhoneAndroid className="w-6 h-8" />
           <span className="text-[19px]">069/1971111</span>
         </a>
         <a
           href="tel:+38119711"
-          className="flex gap-[2px] ml-[120px] items-center border-2 border-cool-green text-cool-green bg-black px-[12px] py-[4px] rounded-full tracking-tight"
+          className="flex gap-[2px] ml-[100px] items-center border-2 border-cool-green text-white bg-black px-[12px] py-[4px] rounded-full tracking-tight"
         >
           <FaPhoneAlt className="w-5 h-6 text-cool-green" />
           <span className="text-[19px]">19711</span>
         </a>
         <a
           href="viber://contact?number=%2B381659719711"
-          className="flex gap-1 ml-[180px] items-center border-2 border-cool-green text-cool-green bg-black px-[12px] py-[4px] rounded-full"
+          className="flex gap-1 ml-[150px] items-center border-2 border-cool-green text-white bg-black px-[12px] py-[4px] rounded-full"
         >
           <FaViber className="bg-purple-600 rounded-full w-6 h-6 p-[3px] text-white" />
           <span className="text-[19px]">065/9719711</span>
@@ -40,7 +40,7 @@ const Hero = () => {
       </div>
 
       <div className="relative z-20">
-        <h1 className="flex flex-col pl-[60px] lg:flex-row lg:gap-8 w-full text-[60px] lg:text-[110px] text-left justify-end pt-[100px] lg:p-[150px] leading-snug text-white">
+        <h1 className="flex flex-col pl-[50px] lg:flex-row lg:gap-8 w-full text-[60px] lg:text-[110px] text-left justify-end pt-[100px] lg:pt-[350px] lg:pr-[250px] leading-snug text-white">
           <span className="text-cool-green font-bold  lg:pr-0">Cool</span>
           Taxi Niš
         </h1>
