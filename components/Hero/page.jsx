f88e2283bcaab00e344145ba6910ada22e19b5cf
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-03 01:53:24 +0100

    .

--- components/Hero/page.jsx
+++ components/Hero/page.jsx
@@ -16,27 +16,27 @@ const Hero = () => {
         }}
       ></div>
       <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
-      <div className=" lg:hidden relative z-20 flex flex-col gap-5 items-start  pb-[15px] pt-[100px]">
+      <div className=" lg:hidden relative z-20 flex flex-col gap-3 items-center  pb-[15px] pt-[100px]">
         <a
           href="tel:+381691971111"
-          className="flex items-center gap-2 ml-[50px] border-2 border-cool-green text-white  px-[12px] py-[4px] rounded-full"
+          className="flex items-center w-[220px] gap-2 border-2 border-cool-green text-white  px-[12px] py-[4px] "
         >
-          <FcPhoneAndroid className="w-6 h-8" />
-          <span className="text-[19px]">069/1971111</span>
+          <FcPhoneAndroid className="w-10 h-10" />
+          <span className="text-[25px]">069/1971111</span>
         </a>
         <a
           href="tel:+38119711"
-          className="flex gap-[8px] ml-[100px] items-center border-2 border-cool-green text-white  px-[12px] py-[4px] rounded-full tracking-tight"
+          className="flex gap-[8px] w-[220px] items-center border-2 border-cool-green text-white  px-[12px] py-[4px] "
         >
-          <FaPhoneAlt className="w-5 h-6 text-cool-green" />
-          <span className="text-[19px]">19711</span>
+          <FaPhoneAlt className="w-10 h-7 text-cool-green" />
+          <span className="text-[25px]">19711</span>
         </a>
         <a
           href="viber://contact?number=%2B381659719711"
-          className="flex gap-3 ml-[150px] items-center border-2 border-cool-green text-white  px-[12px] py-[4px] rounded-full"
+          className="flex gap-3 w-[220px] items-center border-2 border-cool-green text-white  px-[12px] py-[4px] "
         >
-          <FaViber className="bg-purple-600 rounded-full w-6 h-6 p-[3px] text-white" />
-          <span className="text-[19px]">065/9719711</span>
+          <FaViber className="bg-purple-600 rounded-full w-10 h-10 p-[3px] text-white" />
+          <span className="text-[25px]">065/9719711</span>
         </a>
       </div>
       <div className=" relative z-20 text-center pt-[30px]  lg:pt-[200px]">
