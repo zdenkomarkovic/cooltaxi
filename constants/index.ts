4693566e55e5dbc8b7d6e62e5bf569c34e590d7b
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-18 00:44:48 +0100

    .

--- constants/index.ts
+++ constants/index.ts
@@ -54,4 +54,172 @@ export const priceList = [
     pravac: "Ribarska Banja",
     cena: "2.700",
   },
+  {
+    pravac: "Golubac",
+    cena: "16.500",
+  },
+  {
+    pravac: "Srebrno Jezero",
+    cena: "15.000",
+  },
+  {
+    pravac: "Prolom Banja",
+    cena: "4.500",
+  },
+  {
+    pravac: "Lukovska Banja",
+    cena: "5.000",
+  },
+  {
+    pravac: "Prokuplje",
+    cena: "1.800",
+  },
+  {
+    pravac: "Aleksinac",
+    cena: "1.800",
+  },
+  {
+    pravac: "Bela Palanka",
+    cena: "3.500",
+  },
+  {
+    pravac: "Dimitrovgrad",
+    cena: "6.000",
+  },
+  {
+    pravac: "Jagodina",
+    cena: "6.500",
+  },
+  {
+    pravac: "Kruševac",
+    cena: "6.000",
+  },
+  {
+    pravac: "Kraljevo",
+    cena: "8.000",
+  },
+  {
+    pravac: "Svrljig",
+    cena: "1.700",
+  },
+  {
+    pravac: "Bor",
+    cena: "6.500",
+  },
+  {
+    pravac: "Negotin",
+    cena: "7.500",
+  },
+  {
+    pravac: "Paraćin",
+    cena: "5.000",
+  },
+  {
+    pravac: "Ćuprija",
+    cena: "5.500",
+  },
+  {
+    pravac: "Smederevo",
+    cena: "11.000",
+  },
+  {
+    pravac: "Požarevac",
+    cena: "12.000",
+  },
+  {
+    pravac: "Subotica",
+    cena: "26.000",
+  },
+  {
+    pravac: "Šid",
+    cena: "20.000",
+  },
+  {
+    pravac: "Šabac",
+    cena: "20.000",
+  },
+  {
+    pravac: "Kuršumlija",
+    cena: "3.500",
+  },
+  {
+    pravac: "Merdare",
+    cena: "5.000",
+  },
+  {
+    pravac: "Vladičin Han",
+    cena: "4.500",
+  },
+  {
+    pravac: "Ražanj",
+    cena: "4.000",
+  },
+  {
+    pravac: "Novi Pazar",
+    cena: "12.000",
+  },
+  {
+    pravac: "Kladovo",
+    cena: "11.000",
+  },
+  {
+    pravac: "Zaječar",
+    cena: "6.000",
+  },
+  {
+    pravac: "Knjaževac",
+    cena: "3000",
+  },
+  {
+    pravac: "Leskovac",
+    cena: "3000",
+  },
+  {
+    pravac: "Vranje",
+    cena: "7000",
+  },
+  {
+    pravac: "Pirot",
+    cena: "4.000",
+  },
+  {
+    pravac: "Skoplje",
+    cena: "14.000",
+  },
+  {
+    pravac: "Sofija",
+    cena: "12.500",
+  },
+  {
+    pravac: "Sofija Aerodrom",
+    cena: "13.500",
+  },
+  {
+    pravac: "Podgorica",
+    cena: "32.000",
+  },
+  {
+    pravac: "Budva",
+    cena: "38.000",
+  },
+  {
+    pravac: "Priština",
+    cena: "9.000",
+  },
+  {
+    pravac: "Žitorađa",
+    cena: "1.700",
+  },
+  {
+    pravac: "Svilajnac",
+    cena: "9.000",
+  },
+  {
+    pravac: "Pojate",
+    cena: "4.200",
+  },
+  {
+    pravac: "Brus",
+    cena: "4.500",
+  },
 ];
